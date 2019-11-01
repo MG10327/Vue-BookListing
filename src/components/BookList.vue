ba07847bd47aa7f1d83222b3bb1fb14b7de39ec0
MG10327 <54483612+MG10327@users.noreply.github.com>
2019-11-01 19:47:47 -0400

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<style scoped>
+    h1,h2 {font-weight: normal;}
+</style>
+
+<template>
+    <h1>{{title}}</h1>
+
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default Vue.extend({
+        name: 'BookList',
+        data() {
+            return {
+                title: 'All Books'
+            }
+        },
+    })
+</script>
