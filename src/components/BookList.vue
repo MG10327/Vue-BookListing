290b2bb6ca9830c14cd0386e4dbaa72fbfa1cf3b
MG10327 <54483612+MG10327@users.noreply.github.com>
2019-11-01 20:03:00 -0400

    Completed module, adding book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,16 @@
 <style scoped>
     h1,h2 {font-weight: normal;}
+    ul {list-style-type: 'none'; padding: 0;}
+    li {display: block; margin: 0 10px;}
 </style>
 
 <template>
-    <h1>{{title}}</h1>
-
+    <div>
+        <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}, {{book.author}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -12,7 +18,12 @@
         name: 'BookList',
         data() {
             return {
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                            {title: 'American Gods', author: 'Neil Gaiman'},
+                            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                        ]
             }
         },
     })
